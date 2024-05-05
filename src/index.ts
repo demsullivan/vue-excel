2d7060825e6f074808122d271e55b8604ce9344a
Dave Sullivan <dave@davesullivan.ca>
2024-05-05 17:23:24 +0000

    Cleanup, add devcontainer, etc.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { type App, type EmitsOptions, type ComponentOptions, defineComponent } from 'vue'
-import { type ComponentList, type PluginOptions, type MaybeComponent, type ComponentRegistry } from './types'
+import {
+  type ComponentList,
+  type PluginOptions,
+  type MaybeComponent,
+  type ComponentRegistry
+} from './types'
 import VueExcel from './VueExcel'
 import Context from './Context'
 import createWorkbookComponent from './components/Workbook.vue'
@@ -11,8 +16,9 @@ export { VueExcel, Context, Worksheet, Range, Table }
 export type { TableChangedEvent }
 
 function installComponents(app: App, options: PluginOptions): void {
-  const prefix = options.prefix || ""
-  app.component(`${prefix}Workbook`, createWorkbookComponent(options.workbookEmits))
+  const prefix = options.prefix || ''
+  app
+    .component(`${prefix}Workbook`, createWorkbookComponent(options.workbookEmits))
     .component(`${prefix}Worksheet`, Worksheet)
     .component(`${prefix}Range`, Range)
     .component(`${prefix}Table`, Table)
@@ -20,18 +26,23 @@ function installComponents(app: App, options: PluginOptions): void {
 
 function installExcel(app: App, excel: VueExcel): void {
   app.provide('vueExcel', excel)
-  app.config.globalProperties.vueExcel = excel
+  app.provide('vueExcel.context', excel.context)
+  app.config.globalProperties.excel = excel
 }
 
 export async function connectExcel() {
-  if (!window.Office) { throw "Office could not be found! Are you sure you loaded office.js in your index.html?" }
-  if (!Excel) { throw "Excel could not be found!" }
+  if (!window.Office) {
+    throw 'Office could not be found! Are you sure you loaded office.js in your index.html?'
+  }
+  if (!Excel) {
+    throw 'Excel could not be found!'
+  }
 
   return await Excel.run(async (ctx: Excel.RequestContext) => {
     return {
       install(app: App, options: PluginOptions): void {
         const excel = new VueExcel(options, ctx)
-  
+
         installComponents(app, options)
         installExcel(app, excel)
       }
