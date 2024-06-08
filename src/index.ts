92ad0901f537a7db9a365147e89218a89a0454f3
Dave Sullivan <dave@davesullivan.ca>
2024-06-08 15:21:53 +0000

    export Table and Worksheet types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export { type default as Context } from './Context'
 export { connectExcel } from './connect'
 export { type VueExcelGlobalState } from './state'
 export { type CellValue, type FormattedNumber, BaseTransformer, TransformerRegistry } from './data'
+
+export { type default as Table, type TableChangedEvent } from './components/Table.vue'
+export { type default as Worksheet } from './components/Worksheet.vue'
